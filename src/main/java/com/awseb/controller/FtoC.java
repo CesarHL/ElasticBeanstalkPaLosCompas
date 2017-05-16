a6fd2f8b8a1f0e3d4951bc78ac4a165e62b76cdf
CesarHL <cesar.herlop@yahoo.com.mx>
2017-05-15 23:15:27 -0500

    menos codigo

--- src/main/java/com/awseb/controller/FtoC.java
+++ src/main/java/com/awseb/controller/FtoC.java
@@ -1,8 +1,8 @@
 package com.awseb.controller;
 
 import com.awseb.logic.Convertion;
-import com.awseb.model.Result;
 import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,18 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FtoC {
 
+    @Autowired
+    Convertion convertionService;
+
     @RequestMapping(value = "/Temp", method = RequestMethod.GET)
     public ResponseEntity<Optional> convertTemp() {
-        Convertion convertion = new Convertion();
-        Result result = convertion.Convert();
-        return new ResponseEntity(result, HttpStatus.OK);
+
+        return new ResponseEntity(convertionService.convert(), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/Temp/{f}", method = RequestMethod.GET)
     public ResponseEntity<Optional> convertParameter(@PathVariable("f") double f) {
-        Convertion convertion = new Convertion();
-        Result result = convertion.Convert(f);
-        return new ResponseEntity(result, HttpStatus.OK);
+
+        return new ResponseEntity(convertionService.convert(f), HttpStatus.OK);
     }
 
 }
--- src/main/java/com/awseb/logic/Convertion.java
+++ src/main/java/com/awseb/logic/Convertion.java
@@ -1,20 +1,22 @@
 package com.awseb.logic;
 
 import com.awseb.model.Result;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Convertion {
 
     double f = 98.4;
     double c;
 
-    public Result Convert() {
+    public Result convert() {
         c = (f - 32) * 5 / 9;
         Result result = new Result();
         result.setCelcius(c);
         return result;
     }
     
-    public Result Convert(double f) {
+    public Result convert(double f) {
         c = (f - 32) * 5 / 9;
         Result result = new Result();
         result.setCelcius(c);
